161e100649692a4bca8ac7073957639fa5d2f9b4
parkerzhang35 <parkerzhang35@gmail.com>
2024-05-24 11:41:04 -0400

    vue autocomplete

---
+++ charcoal/vite_vue/autocomplete/README.md
@@ -0,0 +1,27 @@
+# autocomplete
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+inspired by 
+    https://www.digitalocean.com/community/tutorials/vuejs-vue-autocomplete-component
+    https://v2.vuejs.org/v2/cookbook/using-axios-to-consume-apis
---
+++ charcoal/vite_vue/autocomplete/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <SearchAutocomplete :items="['Apple',
+    'Banana',
+    'Orange',
+    'Mango'
+  ]" @selected="item = $event"
+  :is-async=false>
+  </SearchAutocomplete>
+  <ui>{{ info }}</ui>
+</template>
+
+<script>
+import SearchAutocomplete from './components/SearchAutocomplete.vue'
+import axios from "axios"
+export default {
+  name: 'App',
+  components: {
+    SearchAutocomplete
+  },
+  data() {
+    return {
+      info :null
+    }
+  },
+  mounted() {
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
