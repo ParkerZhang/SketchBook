c2c6a5ca1b58b02d346f7a4ae4107e5a235c4f45
u <u@localhost.localdomain>
2025-02-10 11:19:08 -0500

    removed from_texts

--- anatomy/LanceDB/app.py
+++ anatomy/LanceDB/app.py
@@ -8,11 +8,25 @@ from langchain_ollama import OllamaLLM
 
 doc = fitz.open("DS.pdf")
 texts, metadatas = [], []
+for page_num in range(len(doc)):
+    page = doc.load_page(page_num)
+    texts.append(page.get_text())
+    metadatas.append(doc.metadata)
+
+
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 document=text_splitter.create_documents(texts,metadatas=metadatas)
 embeddings = OllamaEmbeddings(model="deepseek-r1:14b")
 db = lancedb.connect("/tmp/lancedb")
 vectorstore = LanceDB.from_documents(document, embeddings, connection=db)
+
+#for page_num in range(len(doc)):
+#    page = doc.load_page(page_num)
+#    texts.extend(page.get_text().split("\n"))
+#text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+#vectorstore = LanceDB.from_texts(texts=texts, embedding=embeddings, connection=db)
+
+
 llm = OllamaLLM(model="deepseek-r1:14b")
 retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 4})
 qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
